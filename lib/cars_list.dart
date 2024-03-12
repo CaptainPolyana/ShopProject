2eca6be62c83ab2964913fd5274ee00d1844610c
CaptainPolyana <Danchik557@mail.ru>
2024-03-12 19:10:09 +0300

    first commit

--- lib/cars_list.dart
+++ lib/cars_list.dart
@@ -1,40 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_grid_view/button_to_car.dart';
+import 'package:flutter_grid_view/models/cars.dart';
 class CarsList extends StatelessWidget {
-  const CarsList({Key? key, required this.id_car, required this.name_car, required this.photo_car, required this.compl, required this.cost_car}) : super(key: key);
-  final int id_car;
-  final String name_car;
-  final String photo_car;
-  final String compl;
-  final double cost_car;
+  const CarsList({Key? key, required this.index_car}) : super(key: key);
+  final int index_car;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8),
       decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.all(Radius.circular(16)
-        )
+          color: Colors.grey,
+          borderRadius: BorderRadius.all(Radius.circular(16)
+          )
       ),
       child: Column(
         children: <Widget>[
-            Image.network('${photo_car}',
-                fit: BoxFit.scaleDown,
+          Expanded(flex: 2 ,child: Image.network('${carsList[index_car].photo}',
+            fit: BoxFit.scaleDown,
+          ),
+          ),
+          Center(
+
+            child: Expanded(child: Text('Название автомобиля: ${carsList[index_car].name}'
             ),
-            Center(
-              child:Text('Название автомобиля: ${name_car}'
-              ) ,
-            ),
-            Center(
-              child: Text('Комплектация: ${compl}'
-              ),
-            ),
-            Center(
-              child:Text('Цена: ${cost_car} ₽') ,
             )
+
+          ),
+          Center(
+            child: Expanded(child: Text('Комплектация: ${carsList[index_car].complex}'
+            ),
+            )
+          ),
+          Center(
+            child: Expanded(child: Text('Цена: ${carsList[index_car].cost} ₽')),
+          ),
+          Center(
+              child: Expanded( child: ToCar(index_car: index_car))
+
+
+          )
 
         ],
       ),
-
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,28 +8,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       title:
-       Text('Российские автомобили',
-         style: TextStyle
-           (fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold
-     ),
+      appBar: AppBar(
+        title:
+        Text('Российские автомобили',
+          style: TextStyle
+            (fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold
+          ),
 
-     ),
-     centerTitle: true,
-     ),
-    body: GridView.builder(
-        itemCount: carsList.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-          childAspectRatio: 0.670
         ),
-        itemBuilder: (BuildContext context, int index){
-            return CarsList(id_car: carsList[index].id, name_car: carsList[index].name, photo_car: carsList[index].photo, compl: carsList[index].complex, cost_car: carsList[index].cost);
-        }
-    ),
+        centerTitle: true,
+      ),
+      body: GridView.builder(
+          itemCount: carsList.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 0.610
+          ),
+          itemBuilder: (BuildContext context, int index){
+            return CarsList(index_car: index);
+          }
+      ),
 
     );
   }
